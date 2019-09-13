f48d37b255d1d3e8aaaec868cfac4043d0ac6761
c376W <mailfzc123@gmail.com>
2019-09-13 13:33:47 -0700

    Active class.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,39 +1,42 @@
-var projects=document.querySelectorAll(".project");
-var init_nones=document.querySelectorAll(".init-none");
-var imgs=document.querySelectorAll("img");
+// var projects=document.querySelectorAll(".project");
+// var init_nones=document.querySelectorAll(".init-none");
+// var imgs=document.querySelectorAll("img");
+
 var alinks=document.querySelectorAll(".item");
 var color="#de8787";
 var active=false;
+
 
 alinks.forEach((el)=>{
 	el.addEventListener("click",function(){
 		if(active===true){
 			setColor();
+			active=false;
 		}
 		if(this.style.color!=="#fff"){
-			this.style.color="#fff";
+			this.classList.add("active");
 			active=true;
 		}
 
 	})
 })
 
-projects.forEach((elem,i)=>{
-	elem.addEventListener("mouseover",function(){
-		init_nones[i].style.display="block";
-		      imgs[i].style.display="none";
+// projects.forEach((elem,i)=>{
+// 	elem.addEventListener("mouseover",function(){
+// 		init_nones[i].style.display="block";
+// 		      imgs[i].style.display="none";
 	
-	})
-	elem.addEventListener("mouseleave",function(){
-		init_nones[i].style.display="none";
-		imgs[i].style.display="block";
-	})
-})
+// 	})
+// 	elem.addEventListener("mouseleave",function(){
+// 		init_nones[i].style.display="none";
+// 		imgs[i].style.display="block";
+// 	})
+// })
 
 // Clean up
 
 function setColor(){
 	alinks.forEach(el=>{
-		el.style.color="#de8787";
+		el.classList.remove("active");
 	})
 }
