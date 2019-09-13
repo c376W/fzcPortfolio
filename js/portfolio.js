5561213a28a9bc7d5b2969c3b89ee11cd93d8bc1
c376W <mailfzc123@gmail.com>
2019-09-13 12:26:02 -0700

    Adding active nav effect.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,22 @@
 var projects=document.querySelectorAll(".project");
 var init_nones=document.querySelectorAll(".init-none");
 var imgs=document.querySelectorAll("img");
+var alinks=document.querySelectorAll(".item");
+var color="#de8787";
+var active=false;
+
+alinks.forEach((el)=>{
+	el.addEventListener("click",function(){
+		if(active===true){
+			setColor();
+		}
+		if(this.style.color!=="#fff"){
+			this.style.color="#fff";
+			active=true;
+		}
+
+	})
+})
 
 projects.forEach((elem,i)=>{
 	elem.addEventListener("mouseover",function(){
@@ -13,3 +29,11 @@ projects.forEach((elem,i)=>{
 		imgs[i].style.display="block";
 	})
 })
+
+// Clean up
+
+function setColor(){
+	alinks.forEach(el=>{
+		el.style.color="#de8787";
+	})
+}
