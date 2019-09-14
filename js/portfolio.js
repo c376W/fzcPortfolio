6548a1b45283ff0b0721137995f448ad7ea69709
c376W <mailfzc123@gmail.com>
2019-09-14 11:12:27 -0700

    Adding scrolling effect with active nav link.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,8 @@
-// var projects=document.querySelectorAll(".project");
-// var init_nones=document.querySelectorAll(".init-none");
-// var imgs=document.querySelectorAll("img");
 
 var alinks=document.querySelectorAll(".item");
 var color="#de8787";
 var active=false;
-var c =document.querySelector("canvas");
-document.body.style.background='url('+c.toDataURL()+')';
+var angle=0;//animate the flakes
 
 
 alinks.forEach((el)=>{
@@ -23,87 +19,38 @@ alinks.forEach((el)=>{
 	})
 })
 
-// projects.forEach((elem,i)=>{
-// 	elem.addEventListener("mouseover",function(){
-// 		init_nones[i].style.display="block";
-// 		      imgs[i].style.display="none";
-	
-// 	})
-// 	elem.addEventListener("mouseleave",function(){
-// 		init_nones[i].style.display="none";
-// 		imgs[i].style.display="block";
-// 	})
-// })
 
 //get the canvas and context and store in vars
-	var canvas=document.getElementById("sky");
-	var ctx=canvas.getContext("2d");
+var canvas=document.getElementById("sky");
+var ctx=canvas.getContext("2d");
 
-	//set canvas dims to window height and width
-	var W = window.innerWidth;
-	var H = window.innerHeight;
-	canvas.width=W;
-	canvas.height=H;
+//set canvas dims to window height and width
+var W = window.innerWidth;
+var H = window.innerHeight;
+canvas.width=W;
+canvas.height=H;
 
-	//generate the snowflakes and apply attributes
-	var mf = 100; //max flakes
-	var flakes = [];
+//generate the snowflakes and apply attributes
+var mf = 100; //max flakes
+var flakes = [];
 
-	//loop through the empty flakes and apply attributes
-	for (var i = 0;i<mf;i++){
-		flakes.push({
-			x: Math.random()*W,
-			y: Math.random()*H,
-			r: Math.random()*5+2,//min of 2px and max of 7px
-			d: Math.random()+1 // density of the flake
-		})
-	}
+//loop through the empty flakes and apply attributes
+for (var i = 0;i<mf;i++){
+	flakes.push({
+		x: Math.random()*W,
+		y: Math.random()*H,
+		r: Math.random()*5+2,//min of 2px and max of 7px
+		d: Math.random()+1 // density of the flake
+	})
+}
 
-	//draw flakes onto canvas
-	function drawFlakes(){
-		ctx.clearRect(0,0,W,H);
-		ctx.fillStyle="#888";
-		ctx.beginPath();
-		for(var i=0;i<mf;i++){
-			var f=flakes[i];
-			ctx.moveTo(f.x,f.y);
-			ctx.arc(f.x,f.y,f.r,0,Math.PI*2,true);
-		}
-		ctx.fill();
-		moveFlakes();
-	}
 
-	//animate the flakes
-	var angle=0;
-	function moveFlakes(){
-		angle +=0.01;
-		for(var i=0;i<mf;i++){
-			//store current flake
-			var f= flakes[i];
 
-			//update X and Y coordinates of each snowflake
-			f.y+=Math.pow(f.d,2)+1;
-			// f.y+=1;
-			// f.x+=Math.sin(angle)*2;
+setInterval(drawFlakes,25);
 
-			//if the snowflake reaches the bottom, send a new one to the top
+window.addEventListener('resize',init,false);
 
-			if(f.y>H){
-				flakes[i]={x:Math.random()*W,y:0,r:f.r,d:f.d};
-			}
-		}
-	}
-
-	setInterval(drawFlakes,25);
-
-	window.addEventListener('resize',init,false);
-
-	function init(){
-		var myWidth=window.innerWidth;
-		var myHeight=window.innerHeight;
-		ctx.canvas.width=myWidth;
-		ctx.canvas.height=myHeight;
-	}
+	
 
 // Clean up
 
@@ -111,4 +58,65 @@ function setColor(){
 	alinks.forEach(el=>{
 		el.classList.remove("active");
 	})
+}
+
+function init(){
+	var myWidth=window.innerWidth;
+	var myHeight=window.innerHeight;
+	ctx.canvas.width=myWidth;
+	ctx.canvas.height=myHeight;
+}
+function moveFlakes(){
+	angle +=0.01;
+	for(var i=0;i<mf;i++){
+		//store current flake
+		var f= flakes[i];
+
+		//update X and Y coordinates of each snowflake
+		f.y+=Math.pow(f.d,2)+1;
+		// f.y+=1;
+		// f.x+=Math.sin(angle)*2;
+
+		//if the snowflake reaches the bottom, send a new one to the top
+
+		if(f.y>H){
+			flakes[i]={x:Math.random()*W,y:0,r:f.r,d:f.d};
+		}
+	}
+}
+//draw flakes onto canvas
+function drawFlakes(){
+	ctx.clearRect(0,0,W,H);
+	ctx.fillStyle="#888";
+	ctx.beginPath();
+	for(var i=0;i<mf;i++){
+		var f=flakes[i];
+		ctx.moveTo(f.x,f.y);
+		ctx.arc(f.x,f.y,f.r,0,Math.PI*2,true);
+	}
+	ctx.fill();
+	moveFlakes();
+}
+
+
+// jQuery Section
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.container a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        console.log(refElement.position().top);
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.item').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
 }
